8febfda93464e3cda7f63864295e1de384087fd7
Aslı Çevik <250208007@ogr.alanya.edu.tr>
2026-09-22 10:43:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ print("=== Genetic Sequence Analysis Program ===")
 dna_sequence = input("Enter a DNA sequence: ").upper()
 total_bases = len(dna_sequence)
 
-# Sıfıra bölünme hatasını engellemek için kontrol
+
 if total_bases > 0:
     g_Count = dna_sequence.count('G')
     c_Count = dna_sequence.count('C')
     
-    # GC içerik hesabı
+  
     gc_content = (g_Count + c_Count) / total_bases * 100
     
-    # DNA -> RNA Dönüşümü (Transkripsiyon)
     rna_sequence = dna_sequence.replace('T', 'U')
 
-    # Sonuçları Yazdırma
     print("\n--- ANALYSIS RESULTS ---")
     print(f"Original DNA Sequence: {dna_sequence}")
     print(f"Transcribed RNA Sequence: {rna_sequence}")
@@ -23,7 +21,6 @@ if total_bases > 0:
     print(f"Cytosine (C) count: {c_Count}")
     print(f"GC Content: %{gc_content:.2f}")
 
-    # Başlangıç kodonu kontrolü
     if "ATG" in dna_sequence:
         print("Start Codon (ATG): Found")
     else:
